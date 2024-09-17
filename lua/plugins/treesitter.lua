2d56b5f908debf3ea2c4bbb918b080ea20a71f5f
kamiduki <kamiduki_0@outlook.com>
2024-09-17 22:16:31 +0800

    feat: treesitter textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,25 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects'
+  },
   event = 'User FileOpened',
   main = 'nvim-treesitter.configs',
   build = ":TSUpdate",
   opts = {
+    textobjects = {
+      select = {
+        enable = true,
+        keymaps = {
+          ["af"] = { query = "@function.outer", desc = "function" },
+          ["ac"] = { query = "@class.outer", desc = "class" },
+          ["ab"] = { query = "@block.outer", desc = "block" },
+          ["if"] = { query = "@function.inner", desc = "function" },
+          ["ic"] = { query = "@class.inner", desc = "class" },
+          ["ib"] = { query = "@block.inner", desc = "block" },
+        },
+      },
+    },
     highlight = {
       enable = true,
     },
